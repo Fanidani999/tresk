298f7e3e64b74bf2f6027cc2de7815826ea4b4d5
Fanidani999 <129190547+Fanidani999@users.noreply.github.com>
2023-04-05 07:21:51 +0600

    Update main.py

--- main.py
+++ main.py
@@ -59,11 +59,155 @@
 #
 # finnd_compile_registr =  soup.find_all(text=re.compile("([Оо]дежда)"))
 # print(finnd_compile_registr)
-1-урок
-
-
-
-
+#1-урок
+#
+#
+#
+#
+# # # todo: step 2
+# with open("all_categories_dict.json",encoding="utf-8") as file:
+#  all_categories = json.load(file)
+#
+# iteration_count = int(len(all_categories)) - 1
+# count = 0
+# print(f"ВСЕГО ИТЕРАЦИЙ: {iteration_count}")
+#
+# for category_name, category_href in all_categories.items():
+#
+#     rep = ["'"",", " ", "-"]
+#     for item in rep:
+#         if item in category_name:
+#             category_name = category_name.replace(item, "_")
+#
+#     req = requests.get(url=category_href, headers=headers)
+#     src = req.text
+#
+#     with open(f"data4/{count}_{category_name}.html", "w", encoding="utf-8") as file:
+#         file.write(src)
+#     with open(f"data4/{count}_{category_name}.html", encoding="utf-8") as file:
+#         src = file.read()
+#
+#     soup = BeautifulSoup(src, "lxml")
+#
+#     allert_block =  soup.find(class_="uk-alert-danger")
+#     if allert_block is not None:
+#         continue
+#     # cобираем заголовки таблицы
+#     table_head = soup.find(class_="mzr-tc-group-table").find("tr").find_all("th")
+#     product = table_head[0].text
+#     calories = table_head[1].text
+#     proteins = table_head[2].text
+#     fats = table_head[3].text
+#     carbohydrates = table_head[4].text
+#
+#     with open(f"data4/{count}_{category_name}.csv", "w", encoding="utf-8") as file:
+#         writer = csv.writer(file)
+#         writer.writerow(
+#             (
+#                 product,
+#                 calories,
+#                 proteins,
+#                 fats,
+#                 carbohydrates,
+#             )
+#         )
+#     #sobiraem dannye producta
+#     products_data = soup.find(class_="mzr-tc-group-table").find("tbody").find_all("tr")
+#
+#     product_info = []
+#     for item in products_data:
+#         products_tds = item.find_all("td")
+#
+#         title = products_tds[0].find("a").text
+#         calories = products_tds[1].text
+#         proteins = products_tds[2].text
+#         fats = products_tds[3].text
+#         carbohydrates = products_tds[4].text
+#
+#         product_info.append(
+#             {
+#                 "Title": title,
+#                 "Calories": calories,
+#                 "Proteins": proteins,
+#                 "Fats": fats,
+#                 "Carbohydrates": carbohydrates,
+#             }
+#         )
+#
+#         with open(f"data4/{count}_{category_name}.csv", "a", encoding="utf-8") as file:
+#             writer = csv.writer(file)
+#             writer.writerow(
+#                 (
+#                     title,
+#                     calories,
+#                     proteins,
+#                     fats,
+#                     carbohydrates,
+#                 )
+#             )
+#
+#     with open(f"media9/{count}_{category_name}.json", "a", encoding="utf-8") as  file:
+#         json.dump(product_info, file, indent=4, ensure_ascii=False)
+#     count += 1
+#     print(f"# ИТЕРАЦИЙ {count}. {category_name} ЗАПИСАН...")
+#     iteration_count = iteration_count -1
+#
+#     if iteration_count == 0:
+#         print("РАБОТА ЗАВЕРШЕНА!")
+#         break
+#
+#     print(f"ОСТАЛОСЬ ИНТЕРАЦИЙ: {iteration_count}")
+#     sleep(randint(0,1)) #2-УРОК
+#
+#
+#
+# from random import randint
+# from time import sleep
+# import requests
+# from bs4 import BeautifulSoup
+# import json
+# import csv
+#
+# # # получить код сайта
+# # url = "https://health-diet.ru/table_calorie/"
+# #
+# headers = {
+#     "Accept": "*/*",
+#     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
+# }
+# # req = requests.get(url, headers=headers)
+# # src = req.text
+# # # print(src)
+# #
+# # with open("index.html", "w", encoding="utf-8") as file:
+# #     file.write(src)
+#     #2-урок
+# # получение ссылки
+# # with open("index.html", "r", encoding="utf-8") as file:
+# #     src = file.read()
+# # all_categories_dict = {}
+# # soup = BeautifulSoup(src, "lxml")
+# # all_products_href = soup.find_all(class_="mzr-tc-group-item-href")
+# #
+# # all_categories_dict = {}
+# # for item in all_products_href:
+# #     item_text = item.text
+# #     item_href = "https://health-diet.ru" + item.get("href")
+# #     print(f"{item_text}: {item_href}")
+# #
+# #     all_categories_dict[item_text] = item_href
+# #
+# # with open("all_categories_dict.json", "w", encoding="utf-8") as file:
+# #     json.dump(all_categories_dict, file, indent=4,ensure_ascii=False)
+#
+#       #3-урок
+#
+#
+#
+#
+#
+#
+#
 # import json
 # import time
 # import requests
@@ -184,11 +328,11 @@
 #
 # if _name_ == '_main_':
 #     main()           # 9-урок
-
-
-
-
-
+#
+#
+#
+#
+#
 # import datetime
 # import json
 # import requests
@@ -261,7 +405,7 @@
 #
 #                 }
 #             )
-#         print(f"[INFO] оброботал {page}/{pages_count}")
+#         print(f"[INFO] обработал {page}/{pages_count}")
 #
 #     cur_time = datetime.datetime.now().strftime("%d_%m_%Y_%H_%M")
 #     with open(f"data_{cur_time}.json", "a", encoding="utf-8") as file:
@@ -275,144 +419,9 @@
 #
 # if _name_ == '_main_':
 #     main()
-# #урок-8
-
-
-
-
-# from random import randint
-# from time import sleep
-# import requests
-# from bs4 import BeautifulSoup
-# import json
-# import csv
-#
-# # # получить код сайта
-# # url = "https://health-diet.ru/table_calorie/"
-# #
-# headers = {
-#     "Accept": "*/*",
-#     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
-# }
-# # req = requests.get(url, headers=headers)
-# # src = req.text
-# # # print(src)
-# #
-# # with open("index.html", "w", encoding="utf-8") as file:
-# #     file.write(src)
-#     #2-урок
-# # получение ссылки
-# # with open("index.html", "r", encoding="utf-8") as file:
-# #     src = file.read()
-# # all_categories_dict = {}
-# # soup = BeautifulSoup(src, "lxml")
-# # all_products_href = soup.find_all(class_="mzr-tc-group-item-href")
-# #
-# # all_categories_dict = {}
-# # for item in all_products_href:
-# #     item_text = item.text
-# #     item_href = "https://health-diet.ru" + item.get("href")
-# #     print(f"{item_text}: {item_href}")
-# #
-# #     all_categories_dict[item_text] = item_href
-# #
-# # with open("all_categories_dict.json", "w", encoding="utf-8") as file:
-# #     json.dump(all_categories_dict, file, indent=4,ensure_ascii=False)
-#
-#       #3-урок
+# 8- урок
 #
 #
-# # # todo: step 2
-# with open("all_categories_dict.json",encoding="utf-8") as file:
-#  all_categories = json.load(file)
 #
-# iteration_count = int(len(all_categories)) - 1
-# count = 0
-# print(f"ВСЕГО ИТЕРАЦИЙ: {iteration_count}")
 #
-# for category_name, category_href in all_categories.items():
 #
-#     rep = ["'"",", " ", "-"]
-#     for item in rep:
-#         if item in category_name:
-#             category_name = category_name.replace(item, "_")
-#
-#     req = requests.get(url=category_href, headers=headers)
-#     src = req.text
-#
-#     with open(f"data4/{count}_{category_name}.html", "w", encoding="utf-8") as file:
-#         file.write(src)
-#     with open(f"data4/{count}_{category_name}.html", encoding="utf-8") as file:
-#         src = file.read()
-#
-#     soup = BeautifulSoup(src, "lxml")
-#
-#     allert_block =  soup.find(class_="uk-alert-danger")
-#     if allert_block is not None:
-#         continue
-#     # cобираем заголовки таблицы
-#     table_head = soup.find(class_="mzr-tc-group-table").find("tr").find_all("th")
-#     product = table_head[0].text
-#     calories = table_head[1].text
-#     proteins = table_head[2].text
-#     fats = table_head[3].text
-#     carbohydrates = table_head[4].text
-#
-#     with open(f"data4/{count}_{category_name}.csv", "w", encoding="utf-8") as file:
-#         writer = csv.writer(file)
-#         writer.writerow(
-#             (
-#                 product,
-#                 calories,
-#                 proteins,
-#                 fats,
-#                 carbohydrates,
-#             )
-#         )
-#     #sobiraem dannye producta
-#     products_data = soup.find(class_="mzr-tc-group-table").find("tbody").find_all("tr")
-#
-#     product_info = []
-#     for item in products_data:
-#         products_tds = item.find_all("td")
-#
-#         title = products_tds[0].find("a").text
-#         calories = products_tds[1].text
-#         proteins = products_tds[2].text
-#         fats = products_tds[3].text
-#         carbohydrates = products_tds[4].text
-#
-#         product_info.append(
-#             {
-#                 "Title": title,
-#                 "Calories": calories,
-#                 "Proteins": proteins,
-#                 "Fats": fats,
-#                 "Carbohydrates": carbohydrates,
-#             }
-#         )
-#
-#         with open(f"data4/{count}_{category_name}.csv", "a", encoding="utf-8") as file:
-#             writer = csv.writer(file)
-#             writer.writerow(
-#                 (
-#                     title,
-#                     calories,
-#                     proteins,
-#                     fats,
-#                     carbohydrates,
-#                 )
-#             )
-#
-#     with open(f"media9/{count}_{category_name}.json", "a", encoding="utf-8") as  file:
-#         json.dump(product_info, file, indent=4, ensure_ascii=False)
-#     count += 1
-#     print(f"# ИТЕРАЦИЙ {count}. {category_name} ЗАПИСАН...")
-#     iteration_count = iteration_count -1
-#
-#     if iteration_count == 0:
-#         print("РАБОТА ЗАВЕРШЕНА!")
-#         break
-#
-#     print(f"ОСТАЛОСЬ ИНТЕРАЦИЙ: {iteration_count}")
-#     sleep(randint(0,1)) #2-УРОК
